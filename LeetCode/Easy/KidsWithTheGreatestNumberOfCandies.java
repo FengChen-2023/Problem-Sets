8302db1911b2c40f552238b69555a29146a0dad5
Feng Chen <133109960+FengChen-2023@users.noreply.github.com>
2023-06-27 14:03:13 +0300

    Create KidsWithTheGreatestNumberOfCandies.java

---
+++ LeetCode/Easy/KidsWithTheGreatestNumberOfCandies.java
@@ -0,0 +1,7 @@
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        int max = IntStream.of( candies ).max().orElse( 0 );
+
+        return IntStream.of( candies ).mapToObj( c -> c + extraCandies >= max ).collect( Collectors.toList() );
+    }
+}
