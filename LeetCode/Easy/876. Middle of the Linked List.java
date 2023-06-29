4b9fead31313bfe84b3e1ccbbeb47be25f740c0e
Feng Chen <133109960+FengChen-2023@users.noreply.github.com>
2023-06-29 11:47:53 +0300

    Create 876. Middle of the Linked List.java

---
+++ LeetCode/Easy/876. Middle of the Linked List.java
@@ -0,0 +1,19 @@
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode curr = head;
+        ListNode mid = head;
+
+        while ( curr.next != null )
+        {
+            curr = curr.next;
+            mid = mid.next;
+            
+            if ( curr.next != null )
+            {
+                curr = curr.next;
+            }
+        }
+
+        return mid;
+    }
+}
