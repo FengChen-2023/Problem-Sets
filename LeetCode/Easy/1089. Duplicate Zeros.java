18a2887ed076a4d62cc4cfeef2ebf1443261427e
Feng Chen <133109960+FengChen-2023@users.noreply.github.com>
2023-06-29 13:31:54 +0300

    Update 1089. Duplicate Zeros.java

--- LeetCode/Easy/1089. Duplicate Zeros.java
+++ LeetCode/Easy/1089. Duplicate Zeros.java
@@ -1,5 +1,7 @@
-class Solution {
-    public void duplicateZeros(int[] arr) {
+class Solution
+{
+    public void duplicateZeros( int[] arr )
+    {
         int len = arr.length;
         
         for ( int i = 0; i < len - 1; i++ )
