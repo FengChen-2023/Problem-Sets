1760c0f6bec55342b8fb4c5dca909a819f51bada
Feng Chen <133109960+FengChen-2023@users.noreply.github.com>
2023-06-29 10:58:35 +0300

    Update RootEqualsSumOfChildren.java

--- LeetCode/Easy/RootEqualsSumOfChildren.java
+++ LeetCode/Easy/RootEqualsSumOfChildren.java
@@ -1,5 +1,7 @@
-class Solution {
-    public boolean checkTree(TreeNode root) {
+class Solution
+{
+    public boolean checkTree( TreeNode root )
+    {
         return root.val == root.left.val + root.right.val;
     }
 }
