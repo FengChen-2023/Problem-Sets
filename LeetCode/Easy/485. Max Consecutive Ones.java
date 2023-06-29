746a3416ef01ca56da950437c6ac185f3aec8804
Feng Chen <133109960+FengChen-2023@users.noreply.github.com>
2023-06-29 12:51:46 +0300

    Update 485. Max Consecutive Ones.java

--- LeetCode/Easy/485. Max Consecutive Ones.java
+++ LeetCode/Easy/485. Max Consecutive Ones.java
@@ -1,6 +1,6 @@
 class Solution
 {
-    public int findMaxConsecutiveOnes(int[] nums)
+    public int findMaxConsecutiveOnes( int[] nums )
     {
         int max = 0;
         int len = nums.length;
