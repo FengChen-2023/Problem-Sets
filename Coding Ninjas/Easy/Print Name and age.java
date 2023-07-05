84875f221377ea3c1a93c73216d58e683e4b533d
Feng Chen <133109960+FengChen-2023@users.noreply.github.com>
2023-07-05 10:50:15 +0300

    Create Print Name and age.java

---
+++ Coding Ninjas/Easy/Print Name and age.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Solution
+{
+  	public static void main( String args[] )
+  	{
+    		Scanner sc = new Scanner( System.in );
+
+        System.out.println( "The name of the person is " + sc.next() + " and the age is " + sc.nextInt() + "." );
+	  }
+}
