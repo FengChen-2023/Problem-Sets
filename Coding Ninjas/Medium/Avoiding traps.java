15884dbaf09b22044299b1dd3fd414a2abd5e95e
Feng Chen <133109960+FengChen-2023@users.noreply.github.com>
2023-07-12 10:48:14 +0300

    Update and rename AvoidingTraps.java to Avoiding traps.java

---
+++ Coding Ninjas/Medium/Avoiding traps.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+
+class Solution
+{	
+	public static int avoidTraps( ArrayList<Integer> obstacles, int n )
+	{
+		Collections.sort( obstacles );
+		int last = obstacles.get( obstacles.size() - 1 );
+
+		outter:
+		for ( int i = 2; i < last; i++ )
+		{
+			for ( Integer obstacle : obstacles )
+			{
+				if ( obstacle % i == 0 )
+				{
+					continue outter;
+				}
+			}
+
+			return i;
+		}
+
+		return last + 1;
+	}
+}
